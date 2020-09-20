f8928b2740efec3d5c4a88ce4cb3f2eb083827a2
VadymSkakun <jamal_q@ukr.net>
2020-09-20 21:12:40 +0300

    connect Db, tuning Jackson

--- src/main/java/com/jamal/msg/controller/MessageController.java
+++ src/main/java/com/jamal/msg/controller/MessageController.java
@@ -1,64 +1,56 @@
 package com.jamal.msg.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.jamal.msg.domain.Message;
+import com.jamal.msg.domain.Views;
+import com.jamal.msg.repo.MessageRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.NotActiveException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    private int counter = 4;
+    private final MessageRepo messageRepo;
 
-    private List<Map<String, String>> messages = new ArrayList<>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
-    }};
+    @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    @JsonView(Views.IdName.class)
+    public List<Message> list() {
+        return messageRepo.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getOne(@PathVariable String id) throws NotActiveException {
-        return getMessage(id);
-    }
-
-    private Map<String, String> getMessage(@PathVariable String id) throws NotActiveException {
-        return messages.stream()
-                .filter(messages -> messages.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotActiveException::new);
-    }
-
-    @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-
-        messages.add(message);
-
+    @JsonView(Views.FullMessage.class)
+    public Message getOne(@PathVariable("id") Message message) {
         return message;
     }
 
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
+        return messageRepo.save(message);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) throws NotActiveException {
-        Map<String, String> messageFromDb = getMessage(id);
+    public Message update(
+            @PathVariable("id") Message messageFromDb,
+            @RequestBody Message message
+    ) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
 
-        messageFromDb.putAll(message);
-        messageFromDb.put("id", id);
-
-        return messageFromDb;
+        return messageRepo.save(messageFromDb);
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id) throws NotActiveException {
-        Map<String, String> message = getMessage(id);
-
-        messages.remove(message);
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
     }
 }
